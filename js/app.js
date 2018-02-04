aabd3d923511d5ec91425faa312e2d8d22ffee43
NHollenbeck <nickrhollenbeck@gmail.com>
2018-02-03 21:46:58 -0800

    * 2018-02-03 Created About Page, Pricing Page, & Contact Page

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $('.slider').each(function() {
     var $this = $(this);
     var $group = $this.find('.slide_group');
     var $slides = $this.find('.slide');
+    var $slides_btn = $this.find('.slideBtn .slide');
     var bulletArray = [];
     var currentIndex = 0;
     var timeout;
@@ -74,7 +75,7 @@ $('.slider').each(function() {
       }
     });
     
-    $.each($slides, function(index) {
+    $.each($slides_btn, function(index) {
       var $button = $('<a class="slide_btn">&bull;</a>');
       
       if (index === currentIndex) {
@@ -88,3 +89,112 @@ $('.slider').each(function() {
     
     advance();
   });
+
+
+$('.testimonial').each(function() {
+  var $this = $(this);
+  var $group = $this.find('.slide_group');
+  var $slides = $this.find('.slide');
+  var bulletArray = [];
+  var currentIndex = 0;
+  var timeout;
+  var hello;
+  
+  function move(newIndex) {
+    var animateLeft, slideLeft;
+    
+    advance();
+    
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
+    }
+    
+    bulletArray[currentIndex].removeClass('active');
+    bulletArray[newIndex].addClass('active');
+    
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+    
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft
+    });
+    $group.animate({
+      left: animateLeft
+    }, function() {
+      $slides.eq(currentIndex).css({
+        display: 'none'
+      });
+      $slides.eq(newIndex).css({
+        left: 0
+      });
+      $group.css({
+        left: 0
+      });
+      currentIndex = newIndex;
+    });
+  }
+  
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 4000);
+  }
+  
+  $('.next_btn').on('click', function() {
+    if (currentIndex < ($slides.length - 1)) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+  
+  $('.previous_btn').on('click', function() {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(3);
+    }
+  });
+  
+  advance();
+});
+
+
+//Image Gallery
+$(".imgGallery img").on("click", function () {
+  let modal = $("#popImg");
+  let img = $(this);
+  let modalImg = $("#img01");
+  let imgCaption = $("#caption");
+
+  $(modal).css("display", "block");
+  $(modalImg).attr("src", this.src);
+  $(imgCaption).html(this.alt);
+});
+
+$("#popImg span").on("click", function () {
+    let modal = $("#popImg");
+    $(modal).css("display", "none");
+});
+
+
+$(".sectionCards .col-sm-2").on("click", function(){
+  let $icon = $(this).find("i").attr("id");
+  
+  $(this).addClass("active");
+  $(this).siblings().removeClass("active");
+
+  $(`#${$icon}-div`).removeClass("hide");            
+  $(`#${$icon}-div`).siblings().addClass("hide");
+});
